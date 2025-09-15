1701083fda4e282dd32a87cd9a36f3002e7f4a44
Sherin <sherin.daniel89@yahoo.com>
2025-09-14 21:33:35 -0400

    add

--- App.jsx
+++ App.jsx
@@ -1,3 +1,53 @@
+import { useState } from "react";
+
+import PLANTS from "./data";
+
+import Cart from "./cart/Cart";
+import Plants from "./plants/Plants";
+
+/** Users can add plants to a cart and remove them. */
 export default function App() {
-  return <></>;
+  const [cart, setCart] = useState([]);
+
+  //Finding plant in the cart //
+  const addToCart = (plant) => {
+    const itemExists = cart.find((i) => i.id === plant.id);
+    if (itemExists) {
+      setCart(
+        cart.map((item) =>
+          item.id === plant.id ? { ...item, quantity: item.quantity + 1 } : item
+        )
+      );
+    } else {
+      const item = { ...plant, quantity: 1 };
+      setCart([...cart, item]);
+    }
+  };
+
+  // . map //
+  const removeFromCart = (itemToRemove) => {
+    setCart(
+      cart
+        .map((item) =>
+          item.id === itemToRemove.id
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        )
+        .filter((item) => item.quantity > 0)
+    );
+  };
+
+  return (
+    <>
+      <h1>Proper Plants</h1>
+      <main>
+        <Plants plants={PLANTS} addToCart={addToCart} />
+        <Cart
+          cart={cart}
+          removeFromCart={removeFromCart}
+          addToCart={addToCart}
+        />
+      </main>
+    </>
+  );
 }
